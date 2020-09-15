1b48c4e5ebb9eeca84fd7318e33e9d0e9133fc3f
Fabrizio <fabriziospoto1@gmail.com>
2020-09-15 15:20:44 +0200

    Esercizio Commentato
    Esercizio Commentato

--- index.php
+++ index.php
@@ -3,10 +3,14 @@ Visualizzare a schermo il paragrafo con la relativa
 lunghezza e sostituire la badword passata in GET con tre *. -->
 
 <?php
+// Creo variabile testo
 $text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. Ut quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+// Creo variabile lunghezza con strlen
 $lunghezza = strlen($text);
 
+//Creo variabile GET per badword
 $badword = $_GET['badword'];
+//Sostituisco badword con asterischi
 $replace = str_replace($badword, '***', $text);
 ?>
 
@@ -17,6 +21,7 @@ $replace = str_replace($badword, '***', $text);
         <title>php-badwords</title>
     </head>
     <body>
+        <!-- Stampo a schermo variabile testo normale, lunghezza e variabile testo con asterischi -->
         <p><?php echo $text?></p>
         <p><?php echo $lunghezza?></p>
         <p><?php echo $replace?></p>
